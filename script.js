f0af54b3e5a7ffcf560cf4e185659c97544527f2
Jaša Gregorič <63283597+jasagregoric@users.noreply.github.com>
2024-05-02 16:39:03 +0200

    v1 upload

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
+//import Swal from 'sweetalert2'
 function drawIt() {
-    var x = 350;
-    var y = 750;
     var dx = 0;
-    var dy = 5;
-    var WIDTH=600;
-    var HEIGHT=800;
+    dy = 0;
+    var WIDTH=1000;
+    var HEIGHT=700;
     var r=10;
+    var x = WIDTH/2-r;
+    var y = 650;
     var tocke;
     var start=true;
+    number=3;
+    int=0;
+    var lives=3;
+    var liv=[document.getElementById("img1"),document.getElementById("img2"), document.getElementById("img3")];
+    hit=0;
+    tockeMult=10;
     
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
     
     function init() { 
-      sekunde = 0;
-      izpisTimer = "00:00";
-      intTimer = setInterval(timer, 1000);
       tocke = 0;
-      $("#tocke").html(tocke);
       return intervalId=setInterval(draw, 10);
     }
     
@@ -66,10 +69,10 @@ function drawIt() {
     
     
     function init_paddle() {
-      paddlex = WIDTH / 2;
-      paddley=780;
+      paddlex = WIDTH / 2-60;
+      paddley=690;
       paddleh = 10;
-      paddlew = 75;
+      paddlew = 120;
     }
     
     function draw() {
@@ -117,38 +120,7 @@ function drawIt() {
     $(document).keydown(onKeyDown);
     $(document).keyup(onKeyUp); 
     
-    function draw() {
-      clear();
-      circle(x, y, 10);
-      //premik ploščice levo in desno
-      
-      if (rightDown) paddlex += 5;
-      else if (leftDown) paddlex -= 5;
-      rect(paddlex, paddley, paddlew, paddleh);
-      if (x + dx > WIDTH -r || x + dx < 0 +r)
-        dx = -dx;
-      if (y + dy < 0 +r)
-        dy = -dy;
-      else if (y + dy > HEIGHT -r) {
-        if (x > paddlex && x < paddlex + paddlew) {
-            dx = 8 * ((x-(paddlex+paddlew/2))/paddlew);
-            dy = -dy;
-          }
-          else if (y + dy > HEIGHT -r) {
-            start=false;
-                if (x > paddlex && x < paddlex + paddlew){
-            dx = 8 * ((x-(paddlex+paddlew/2))/paddlew);
-            dy = -dy;
-            start=true;
-            }
-                else if (y + dy > HEIGHT-r)
-                  clearInterval(intervalId);
-             
-              }
-            }
-      x += dx;
-      y += dy;
-    }
+    
     var canvasMinX;
     var canvasMaxX;
     
@@ -169,17 +141,14 @@ function drawIt() {
     init_mouse();
     
     var bricks;
-    var NROWS;
-    var NCOLS;
+    var NROWS = 3;
+    var NCOLS = 4;
     var BRICKWIDTH;
-    var BRICKHEIGHT;
+    var BRICKHEIGHT = 40;
     var PADDING;
     
     function initbricks() { //inicializacija opek - polnjenje v tabelo
-      NROWS = 10;
-      NCOLS = 13;
       BRICKWIDTH = (WIDTH/NCOLS) - 1;
-      BRICKHEIGHT = 15;
       PADDING = 1;
       bricks = new Array(NROWS);
       for (i=0; i < NROWS; i++) {
@@ -190,63 +159,6 @@ function drawIt() {
       }
     }
     initbricks();
-    
-    function draw() {
-        clear();
-        circle(x, y, 10);
-        //premik ploščice levo in desno
-        if(rightDown){
-          if((paddlex+paddlew) < WIDTH){
-          paddlex += 5;
-          }else{
-          paddlex = WIDTH-paddlew;
-          }
-          }
-          else if(leftDown){
-          if(paddlex>0){
-          paddlex -=5;
-          }else{
-          paddlex=0;
-          }
-          }
-      
-      rect(paddlex, paddley, paddlew, paddleh);
-      
-      //riši opeke
-        for (i=0; i < NROWS; i++) {
-          for (j=0; j < NCOLS; j++) {
-            if (bricks[i][j] == 1) {
-              rect((j * (BRICKWIDTH + PADDING)) + PADDING,
-                  (i * (BRICKHEIGHT + PADDING)) + PADDING,
-                  BRICKWIDTH, BRICKHEIGHT);
-            }
-          }
-        }
-      
-        if (x + dx > WIDTH -r || x + dx < r)
-          dx = -dx;
-        if (y + dy < r)
-          dy = -dy;
-        else if (y + dy > HEIGHT -(r)) {
-            if (x > paddlex && x < paddlex + paddlew) {
-                dx = 8 * ((x-(paddlex+paddlew/2))/paddlew);
-                dy = -dy;
-              }
-              else if (y + dy > HEIGHT -r) {
-                start=false;
-                    if (x > paddlex && x < paddlex + paddlew){
-                dx = 8 * ((x-(paddlex+paddlew/2))/paddlew);
-                dy = -dy;
-                start=true;
-                }
-                    else if (y + dy > HEIGHT-r)
-                      clearInterval(intervalId);
-                 
-                  }
-        }
-        x += dx;
-        y += dy;
-      }
     
       function draw() {
         clear();
@@ -272,6 +184,7 @@ function drawIt() {
         for (i=0; i < NROWS; i++) {
           for (j=0; j < NCOLS; j++) {
             if (bricks[i][j] == 1) {
+              ctx.fillStyle="#2ec72c"
               rect((j * (BRICKWIDTH + PADDING)) + PADDING,
                   (i * (BRICKHEIGHT + PADDING)) + PADDING,
                   BRICKWIDTH, BRICKHEIGHT);
@@ -285,10 +198,33 @@ function drawIt() {
         col = Math.floor(x/colwidth);
         //Če smo zadeli opeko, vrni povratno kroglo in označi v tabeli, da opeke ni več
         //štetje zadetih opek
-        if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 1) {
-        dy = -dy; bricks[row][col] = 0;
-        tocke += 1; //v primeru, da imajo opeko večjo utež lahko prištevate tudi npr. 2 ali 3; pred tem bi bilo smiselno dodati še kakšen pogoj, ki bi signaliziral mesta opek, ki imajo višjo vrednost
-        $("#tocke").html(tocke);
+        if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 1){
+          dy = -dy; bricks[row][col] = 0;
+          hit += 1;
+          tocke+=(1*tockeMult)
+          if(hit==NCOLS*NROWS){
+            if(lives<3){
+              liv[lives].style.visibility = 'visible';
+              lives++;
+            }
+            dx = 0;
+            dy=0;
+            tockeMult+=10;
+            int+=0.5;
+            number=3;
+            startGame();
+            x = WIDTH/2-r;
+            y = 650;
+            NCOLS++;
+            if(NCOLS>5 && NCOLS%3==0){
+              NROWS++;
+              BRICKHEIGHT=120/NROWS;
+            }
+            initbricks();
+            init_paddle()
+            hit=0;
+          }
+          $("#tocke").html(tocke);
         }
     
         if (x + dx > WIDTH -r || x + dx < r)
@@ -307,36 +243,72 @@ function drawIt() {
               dy = -dy;
               start=true;
               }
-                  else if (y + dy > HEIGHT-r)
-                    clearInterval(intervalId);
-               
+                  else if (y + dy > HEIGHT-r){
+                    lives--;
+                    if(lives!=-1){
+                      liv[lives].style.visibility = 'hidden';
+                      dy=0;
+                      dx=0;
+                      number=3;
+                      startGame();
+                      x = WIDTH/2-r;
+                      y = 650;
+                      init_paddle()
+                    }else{
+                      clearInterval(intervalId);
+                      end(tocke);
+                    }
+                  }
                 }
         }
         x += dx;
         y += dy;
       }
-        //timer
-      var sekunde;
-      var sekundeI;
-      var minuteI;
-      var intTimer;
-      var izpisTimer;
-      //timer
-      function timer(){
-        if(start==true){
-          sekunde++;
-          
-          sekundeI = ((sekundeI = (sekunde % 60)) > 9) ? sekundeI : "0"+sekundeI;
-          minuteI = ((minuteI = Math.floor(sekunde / 60)) > 9) ? minuteI : "0"+minuteI;
-          izpisTimer = minuteI + ":" + sekundeI;
-          
-          $("#cas").html(izpisTimer);
-          }
-          else{
-          sekunde=0;
-          //izpisTimer = "00:00";
-          $("#cas").html(izpisTimer);
-          }
-        }
-      timer();
     }
+document.getElementById("startBtn").addEventListener("click", startGame);
+
+function startGame(){
+  document.getElementById("startBtn").disabled = true;
+  document.getElementById("number").innerHTML=number;
+  number--;
+  inter=setInterval(displayNumber, "1000", number==-1);
+  setTimeout(() => {
+    dy=3+int;
+  }, "3000");
+}
+function displayNumber(){
+  document.getElementById("number").innerHTML=number;
+  number--;
+  if(number==-1){
+    clearInterval(inter);
+    document.getElementById("number").innerHTML=""
+  }
+}
+function end(tocke){
+  Swal.fire({
+    title: 'Congratulations! You have managed to get '+tocke+' points.',
+    //text: '',
+    confirmButtonText: 'COOL',
+    icon: 'success',
+    background: 'black',
+    color: '#2ec72c',
+    confirmButtonColor: '#2ec72c',
+    iconColor: '#2ec72c'
+  }).then(function(){ 
+    location.reload();
+  }
+ );
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  Swal.fire({
+    title: 'Wecome!',
+    text: 'Your objective is to destroy as many bricks as you can to break into NSSAs server. You have 3 lives and if you\'ve lost a heart you\'ll recive another heart after compleating a level. Press the START button to begin. Good luck!',
+    confirmButtonText: 'I\'M READY',
+    icon: 'info',
+    background: 'black',
+    color: '#2ec72c',
+    confirmButtonColor: '#2ec72c',
+    iconColor: '#2ec72c'
+  })
+});
